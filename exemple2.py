66bdb92170e5bc166b8e6e4920aafe5f3ae6cfa9
BeanOfChaos <benjarte@ulb.ac.be>
2020-02-17 17:36:33 +0100

    ez modularity

--- exemple2.py
+++ exemple2.py
@@ -2,34 +2,41 @@
 """
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import Adam
 
 import matplotlib.pyplot as plt
 
 DS_SIZE = 1000
 BATCH_SIZE = 250
+N_EPOCHS = 1000
+LR = 0.1
+DENSE_UNITS = 30
+
 
 if __name__ == "__main__":
     xs = np.random.random_sample(size=(DS_SIZE, 1)) * 2 * np.pi
     ys = np.sin(xs)
 
     model = Sequential()
-    model.add(Dense(30, activation="tanh", input_shape=(1,)))
+    model.add(Dense(DENSE_UNITS, activation="tanh", input_shape=(1,)))
+    model.add(Dropout(0))
     model.add(Dense(1))
+    model.add(Dropout(0))
     model.add(Activation("linear"))
 
-    opt = Adam(lr=0.1)
+    opt = Adam(lr=LR)
 
     model.compile(optimizer=opt,
                   loss='mse',
                   metrics=["mae"])
 
     model.fit(xs, ys,
-              epochs=1000,
+              epochs=N_EPOCHS,
               steps_per_epoch=DS_SIZE//BATCH_SIZE,
               batch_size=BATCH_SIZE,
               )
+
     test_x = np.arange(0, 15, 0.1)
     test_y = np.sin(test_x)
 
